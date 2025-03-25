2535f324d5e6454c06ce8a3b348f31851a5bfe51
tracyn09 <tracyn0099@gmail.com>
2025-03-25 11:31:51 -0400

    Task 3–Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -16,3 +16,25 @@ function fetchProductsThen() {
         console.error(`Error`, error)
     })
 }
+
+//Task 3
+    //Fetching products with async/await
+async function fetchProductsAsync() {
+    try {
+        const output = await fetch(`https://www.course-api.com/javascript-store-products`)
+        const data = await output.json()
+        displayProducts(data)
+    }
+    catch(error) {
+        handleError(error)
+    }
+}
+    //Helper function to render products on page
+    function displayProducts(products) {
+        products.forEach(product => {console.log(product.fields.name)})
+    }
+
+    //Handle error
+    function handleError(error) {
+        console.error(`Error`)
+    }
